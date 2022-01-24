8a98b04dedfdb7ccfbbc4244f9cc67e2ed9d5b4a
Dygenlow <72850183+Dygenlow@users.noreply.github.com>
2022-01-24 21:33:32 +0800

    -Added Win & Lose Conditions
    -Win Condition: Player collects 4 coins around the map to win
    
    -Lose Condition; Player loses if they touch any of the Hazard Walls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private int coins;
     public float speed;
     Rigidbody PlayerRigidbody;
+    public Text coinsCollected;
     
     // Start is called before the first frame update
     void Start()
@@ -21,5 +24,25 @@ public class PlayerMovement : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
         PlayerRigidbody.AddForce(movement * speed * Time.deltaTime);
+
+        if(coins >= 4)
+        {
+            SceneManager.LoadScene("GameWin");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag.Equals("Hazard"))
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+
+        if(collision.gameObject.tag.Equals("Coin"))
+        {
+            coins++;
+            coinsCollected.GetComponent<Text>().text = "Coins Collected: " + coins;
+            Destroy(collision.gameObject);
+        }
     }
 }
