d04dd431b7dd461756e88cdd0add64b7325e0019
Dygenlow <72850183+Dygenlow@users.noreply.github.com>
2022-01-24 21:46:23 +0800

    -Added Coin Rotation
    -Added Coin Rotation at 90 Degree X-Axis

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
+    public float rotateSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.rotation = Quaternion.Euler(90, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
     }
 }
